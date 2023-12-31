2dacf5c8b3ebbb0109858f91b9f222d477b614ac
alaatarek96 <alaaelhelw094@gmail.com>
2023-12-31 22:02:38 +0200

    design post models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+''' 
+        1 : html widget
+        2 : validation 
+        3 : best for db 
+'''
+
 # Create your models here.
+
+''' 
+    post :
+        -title
+        -content
+        -draft
+        -publish-date
+        -author
+        -image
+        -tags
+        -category
+        -comments
+'''
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=20000)
+    draft = models.BooleanField(default=True)
